958f5dea13f519cc7b051a6c69a5e16b82e50b47
Peniel Bekele <142409326+Penie1@users.noreply.github.com>
2024-08-17 02:05:35 +0300

    Add Footer component for layout consistency

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+export default function Footer() {
+  return (
+    <div className="py-4 bg-gray-900">
+      <h2 className=" text-gray-400 text-center">
+        Created by:{" "}
+        <a
+          className="text-gray-100 underline hover:no-underline hover:text-cyan-600"
+          href="https://github.com/Penie1"
+          target="_blank"
+        >
+          Peniel Bekele
+        </a>
+      </h2>
+    </div>
+  );
+}
