f20871d51731d2fcde4d62b61af48ca235739ea9
Peniel Bekele <142409326+Penie1@users.noreply.github.com>
2024-08-17 00:00:11 +0300

    Implement error handling for async functions

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,15 @@
 export async function fetchData() {
-  const url = "https://rickandmortyapi.com/api/character/1,2,3,4,5,6,7,8,9";
-  const data = await fetch(url);
-  const result = await data.json();
-  return result;
+  try {
+    const url = "https://rickandmortyapi.com/api/character/1,2,3,4,5,6,7,8,9";
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    alert("Failed to fetch data:", error);
+    return [];
+  }
 }
