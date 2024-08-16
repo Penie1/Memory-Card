9f0ce9ee1c037c83d9ef61fb22c2f8b5a698ce19
Peniel Bekele <142409326+Penie1@users.noreply.github.com>
2024-08-17 01:06:19 +0300

    Update text color and alignment

--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -2,10 +2,10 @@ import PropTypes from "prop-types";
 
 export default function Scoreboard({ currentScore, bestScore }) {
   return (
-    <p className="flex flex-col text-white text-right pr-10 text-2xl">
-      <span>Current score: {currentScore}</span>
-      <span>Best score: {bestScore}</span>
-    </p>
+    <div className="flex flex-col pl-10 text-2xl">
+      <span className="text-slate-100">Current score: {currentScore}</span>
+      <span className="text-green-500">Best score: {bestScore}</span>
+    </div>
   );
 }
 
