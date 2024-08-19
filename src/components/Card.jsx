f9672da887f5a644b9b80a622ca551d793a34148
Peniel Bekele <142409326+Penie1@users.noreply.github.com>
2024-08-19 12:05:51 +0300

    Improve UI responsiveness across devices

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,10 +3,10 @@ export default function Card({ image, name, onClick }) {
   return (
     <div
       onClick={onClick}
-      className="flex flex-col cursor-pointer gap-5 bg-gray-900  rounded-xl overflow-hidden font-creepster max-w-64 pb-4"
+      className="flex flex-col cursor-pointer gap-5 bg-gray-900  rounded-xl overflow-hidden font-creepster max-w-64 pb-4 "
     >
       <img src={image} alt={name} />
-      <h2 className="text-cyan-500 text-center  md:text-2xl text-base tracking-wider ">
+      <h2 className="text-cyan-500 text-center  md:text-2xl text-base tracking-wider px-1">
         {name.toUpperCase()}
       </h2>
     </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,9 @@ import Scoreboard from "./Scoreboard";
 export default function Header({ currentScore, bestScore }) {
   return (
     <header className="flex flex-col gap-8 py-5 font-creepster">
-      <h1 className="text-green-500  text-center text-6xl">Memory Card</h1>
+      <h1 className="text-green-500  text-center md:text-6xl text-5xl">
+        Memory Card
+      </h1>
       <p className="text-gray-900 text-center text-2xl md:block hidden">
         Get points by clicking on an image but don&apos;t click on any more than
         once!
